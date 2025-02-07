32f9d394547a69431a1a1110ee6f16068d8dca69
Paola Sonia <p.huaripayta@gmail.com>
2025-02-07 18:58:07 -0500

    Proyecto 0 -version 2

--- script.js
+++ script.js
@@ -1,39 +1,18 @@
-function cambiarTexto(){
-    document.getElementById('titulo').innerText = "¡Texto cambiado con Javascript!"
-}
-
-function verificarEdad(){
-    let edad = document.getElementById("edad").value;
-
-    if(edad > 18){
-        console.log(edad, "mayor de edad");
-        document.getElementById('respuesta').innerHTML = "Eres mayor de edad" + edad;
-    } else{
-        console.log(edad, "MENOR de edad");
-        document.getElementById('respuesta').innerHTML = "Eres MENOR de edad" + edad;
-    }
-    
-}
-
-
-function nroEnteroAleatorio(){
-    min = Math.ceil(1)
-    max = Math.floor(5)
-    let numeroAleatorio = Math.floor(Math.random() * (max - min)) + min;
-    return numeroAleatorio;
-}
-
 function coincidencia(){
-    let numeroAleatorio = nroEnteroAleatorio();
+    let numeroAleatorio = Math.floor(Math.random() * (5 - 1)) + 1;
     let numeroIngresado = document.getElementById("numeroIngresado").value;
     let respuesta = "";
     
-    if (numeroIngresado != numeroAleatorio) {
+    if (numeroIngresado > numeroAleatorio) {
         // console.log("El nro ingresado: " + numeroIngresado + " NO es igual a: " + numeroAleatorio);
-        respuesta = "El nro ingresado: " + numeroIngresado + " NO es igual a: " + numeroAleatorio;
-    } else {
+        respuesta = "El nro ingresado: " + numeroIngresado + " es MAYOR al: " + numeroAleatorio;
+    } 
+    if (numeroIngresado < numeroAleatorio){
+        respuesta = "El nro ingresado: " + numeroIngresado + " es MENOR al: " + numeroAleatorio;
+    }
+    if (numeroIngresado == numeroAleatorio) {
         //console.log("El nro ingresado: " + numeroIngresado + " SI es igual a: " + numeroAleatorio);
-        respuesta = "El nro ingresado: " + numeroIngresado + " SI es igual a: " + numeroAleatorio;
+        respuesta = "ADIVINASTE El nro ingresado: " + numeroIngresado + " es IGUAL al: " + numeroAleatorio;
     }
 
     document.getElementById('respuesta').innerHTML = respuesta;
